a303bacccae34881b01635de61a8c33f683e06f1
karupanerura <karupa@cpan.org>
2018-12-19 17:47:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+IO::Concurrent - It's new $module
+
+# SYNOPSIS
+
+    use IO::Concurrent;
+
+# DESCRIPTION
+
+IO::Concurrent is ...
+
+# LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+karupanerura <karupa@cpan.org>
---
+++ lib/IO/Concurrent.pm
@@ -0,0 +1,39 @@
+package IO::Concurrent;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+IO::Concurrent - It's new $module
+
+=head1 SYNOPSIS
+
+    use IO::Concurrent;
+
+=head1 DESCRIPTION
+
+IO::Concurrent is ...
+
+=head1 LICENSE
+
+Copyright (C) karupanerura.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+karupanerura E<lt>karupa@cpan.orgE<gt>
+
+=cut
+
